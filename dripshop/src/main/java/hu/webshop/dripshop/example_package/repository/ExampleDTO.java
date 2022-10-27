356f4ac8aa3865ffaf4fece23a51dbe16cf5d0bb
Pal Szilard <h050611@stud.u-szeged.hu>
2022-10-27 22:43:31 +0200

    Add examples

--- dripshop/src/main/java/hu/webshop/dripshop/example_package/repository/ExampleDTO.java
+++ dripshop/src/main/java/hu/webshop/dripshop/example_package/repository/ExampleDTO.java
@@ -1,12 +1,35 @@
 package hu.webshop.dripshop.example_package.repository;
 
+import hu.webshop.dripshop.example_package.model.Example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ExampleDTO implements ExampleRepository {
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
-	@Override
-	public String getExampleData() {
-		return "Ahol a pacek a still";
+@Repository
+public class ExampleDTO extends JdbcDaoSupport {
+
+	@Autowired
+	DataSource dataSource;
+
+	@PostConstruct
+	private void initialize() {
+		setDataSource(dataSource);
+	}
+
+	public void createExample(String name) {
+		String sql = "INSERT INTO example(name) VALUES(?)";
+		getJdbcTemplate().update(sql, name);
+	}
+
+	public String get() {
+		String sql = "SELECT * FROM example";
+		List<Map<String, Object>> rows = getJdbcTemplate().queryForList(sql);
+
+		return (String) rows.get(0).get("name");
 	}
 }
--- dripshop/src/main/java/hu/webshop/dripshop/example_package/repository/ExampleRepository.java
+++
@@ -1,8 +0,0 @@
-package hu.webshop.dripshop.example_package.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExampleRepository {
-	String getExampleData();
-}
