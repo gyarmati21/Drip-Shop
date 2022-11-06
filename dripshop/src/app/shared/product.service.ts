ea49acf515c58063aeebb61172d9eb91cfbed3c4
Györök Gábor <h158138@stud.u-szeged.hu>
2022-11-06 09:59:45 +0100

    Product managment methods

--- dripshop/src/app/shared/product.service.ts
+++ dripshop/src/app/shared/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, resolveForwardRef } from '@angular/core';
 import { Product } from "./product.module";
 import { AngularFirestore } from "@angular/fire/compat/firestore";
 
@@ -12,5 +12,35 @@ export class ProductService {
 
   getProducts(){
     return this.firestore.collection("product").snapshotChanges();
+  }
+
+  //Product managment
+
+  createProduct(data : Product){
+    return new Promise<any>((resolve, reject) => {
+      this.firestore
+        .collection("products")
+        .add(data)
+        .then(res => {resolve(res)}, err => reject(err));
+    });
+  }
+
+  updateProduct(data: Product){
+    let productId = data.id;
+
+    return new Promise<any>((resolve, reject) => {
+      this.firestore.collection("products")
+        .doc(productId)
+        .update(data)
+        .then(res => {resolve(res)}, err => reject(err));
+    });
+  }
+
+  deleteProduct(data: Product){
+    return new Promise<any>((resolve,reject) => {
+      this.firestore.collection("products")
+        .doc(data.id).delete()
+        .then(res => {resolve(res)}, err => reject(err));
+    });
   }
 }
