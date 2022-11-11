ec7e68d7a4473b82fe57d54f627ccd01dba98461
Rózsa Patrik Zsolt <h163488@stud.u-szeged.hu>
2022-11-11 20:13:04 +0100

    user management html table

--- dripshop/src/app/app.module.ts
+++ dripshop/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 // FireStore module (Database)
 import { AngularFireModule } from "@angular/fire/compat";
