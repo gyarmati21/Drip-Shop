0d6e7c6e31d570a7d760d321a46432514059638e
Kabók Dániel Tibor <h046510@stud.u-szeged.hu>
2022-11-14 20:02:02 +0100

    Cart component ts basic frame

--- dripshop/src/app/app-routing.module.ts
+++ dripshop/src/app/app-routing.module.ts
@@ -11,12 +11,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
   {path: 'home', component:HomeComponent},
   {path: 'login', component:LoginComponent},
-  {path: 'sign-up', component:SignupComponent}
+  {path: 'sign-up', component:SignupComponent},
+  {path: 'cart', component:CartComponent}
+
 ];
 
 @NgModule({
--- dripshop/src/app/cart/cart.component.ts
+++ dripshop/src/app/cart/cart.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../shared/cart-item.module';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router, private userv: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  getCartItems() {
+    
+  }
+
+  deleteCartItem(item : CartItem) {
+
+  }
+
+  calculatePrice() {
+
+  }
+
+  onSubmit() {
+    this.router.navigateByUrl("/order");
   }
 
 }
