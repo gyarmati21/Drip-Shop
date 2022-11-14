51368e96b421eb297780ebce9a5ef3cfa5737df6
Kücüktüfekci Cengiz Etem <h160842@stud.u-szeged.hu>
2022-11-14 23:11:38 +0100

    Property order module

--- dripshop/src/app/shared/order.module.ts
+++ dripshop/src/app/shared/order.module.ts
@@ -1,12 +1,4 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
-
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
-})
-export class OrderModule { }
+export class Order { 
+	user: User;
+	date: DateTime;
+}
