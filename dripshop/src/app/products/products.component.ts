4ae2bec0d299cd9e5000b84c31e722aab413a86c
Katona Ádám <h159532@stud.u-szeged.hu>
2022-11-14 19:16:35 +0100

    Opening one item added

--- dripshop/src/app/products/products.component.ts
+++ dripshop/src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { Product } from "../shared/models/product.model";
 import { ProductService } from "../shared/product.service";
+import { ProductDialogComponent } from "./product-dialog/product-dialog.component";
 
 @Component({
   selector: "app-products",
@@ -10,11 +12,21 @@ import { ProductService } from "../shared/product.service";
 export class ProductsComponent implements OnInit {
   productList: Product[];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((actionArray) => {
       this.productList = actionArray;
+    });
+  }
+
+  openProductDialog(product: Product): void {
+    const dialogRef = this.dialog.open(ProductDialogComponent, {
+      width: "250px",
+      data: product,
     });
   }
 }
