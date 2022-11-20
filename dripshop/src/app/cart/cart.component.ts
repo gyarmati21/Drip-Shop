9533654575d30575ca905e4800b056d19ce0c66a
Kabók Dániel Tibor <h046510@stud.u-szeged.hu>
2022-11-20 23:42:21 +0100

    Testing adding to cart bases

--- dripshop/src/app/cart/cart.component.ts
+++ dripshop/src/app/cart/cart.component.ts
@@ -3,6 +3,8 @@ import { CartItem } from '../shared/cart-item.module';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { SizesObject } from '../shared/sizes'; 
+import { Product } from '../shared/models/product.model';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-cart',
@@ -16,13 +18,33 @@ export class CartComponent implements OnInit {
   sizes = SizesObject;
 
 
-  constructor(private router : Router, private userv: AuthenticationService) { }
+  constructor(private router : Router, private userv: AuthenticationService, private productService: ProductService) { }
 
+
+  productList: Product[] = [];
+  cartItemList: CartItem[] = [];
   ngOnInit(): void {
+    this.productService.getProducts().subscribe((actionArray) => {
+      this.productList = actionArray;
+    });
+
+    console.log(this.productList);
+
+    for (let index = 0; index < this.productList.length; index++) {
+      this.cartItemList[index].product = this.productList[index];
+      this.cartItemList[index].size = "XL";
+      this.cartItemList[index].quantity = 1;
+    }
+
+    console.log(this.userv.cartContent);
   }
 
   getCartItems() {
-    
+    return this.userv.cartContent;
+  }
+
+  addCartItem(item: CartItem) {
+    this.userv.cartContent.push(item);
   }
 
   deleteCartItem() { /*deleteCartItem(item : CartItem)*/
