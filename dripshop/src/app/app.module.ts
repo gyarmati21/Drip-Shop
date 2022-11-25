6de700fb40a40e7ab33feff53dd046cf11daad5a
Pal Szilard <h050611@stud.u-szeged.hu>
2022-11-25 19:42:40 +0100

    Add  fromtend to the product's filtering

--- dripshop/src/app/app.module.ts
+++ dripshop/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -77,8 +78,8 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
     FormsModule,
     MatInputModule,
     HotToastModule.forRoot(),
-    MatDialogModule
-    
+    MatDialogModule,
+    MatCheckboxModule,
   ],
   providers: [MatDialogModule],
   bootstrap: [AppComponent]
--- dripshop/src/app/products/products.component.ts
+++ dripshop/src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { MatCheckboxChange } from "@angular/material/checkbox";
+import { _countGroupLabelsBeforeOption } from "@angular/material/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Product } from "../shared/models/product.model";
 import { ProductService } from "../shared/product.service";
@@ -10,7 +12,9 @@ import { ProductDialogComponent } from "./product-dialog/product-dialog.componen
   styleUrls: ["./products.component.scss"],
 })
 export class ProductsComponent implements OnInit {
-  productList: Product[];
+  productList: Product[] = [];
+  categories: String[] = [];
+  selectedCategories: String[] = [];
 
   constructor(
     private productService: ProductService,
@@ -21,6 +25,9 @@ export class ProductsComponent implements OnInit {
     this.productService.getProducts().subscribe((actionArray) => {
       this.productList = actionArray;
     });
+    this.productService.getCategories().subscribe((category) => {
+      this.categories.push(category);
+    });
   }
 
   openProductDialog(product: Product): void {
@@ -28,4 +35,25 @@ export class ProductsComponent implements OnInit {
       data: product,
     });
   }
+
+  filter() {
+    this.productList = [];
+    this.selectedCategories.forEach(category => {
+      return this.productService.getProductsByCategory(category)
+        .subscribe(product => this.productList.push(product));
+    });
+  }
+
+  changeCategorySelectedState(event: MatCheckboxChange) {
+    if (event.checked == true) {
+      this.selectedCategories.push(event.source.value)
+    }
+    else {
+      const index = this.selectedCategories.indexOf(event.source.value, 0);
+      if (index > -1) {
+        this.selectedCategories.splice(index, 1);
+      }
+    }
+  }
+
 }
