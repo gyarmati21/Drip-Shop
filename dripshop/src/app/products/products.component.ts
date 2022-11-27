c323883fecc1cdadc9ab1653ac761a75c9728881
Katona Ádám <h159532@stud.u-szeged.hu>
2022-11-27 17:01:19 +0100

    One product page remake

--- dripshop/src/app/products/products.component.ts
+++ dripshop/src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
+import { ɵINTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS } from "@angular/platform-browser-dynamic";
 import { Product } from "../shared/models/product.model";
 import { ProductService } from "../shared/product.service";
 import { ProductDialogComponent } from "./product-dialog/product-dialog.component";
@@ -25,6 +26,7 @@ export class ProductsComponent implements OnInit {
 
   openProductDialog(product: Product): void {
     const dialogRef = this.dialog.open(ProductDialogComponent, {
+      backdropClass: "dialog-class",
       data: product,
     });
   }
